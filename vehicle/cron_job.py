72f9f9340e68dc9186071633c3dd24061bc03171
zafarkhan-30 <pubgvila301999@gmail.com>
2024-05-19 15:02:56 +0530

    updated cronjob file

--- vehicle/cron_job.py
+++ vehicle/cron_job.py
@@ -1,13 +1,17 @@
 import requests
 
 def run_device_details_view():
-    url = "https://timscan.transvolt.in/vehical/post-Device-Details/"  
-    headers = {"Content-Type": "application/json"} 
-    response = requests.get(url, headers=headers)
-    
-    if response.status_code == 200:
-        print("Device details successfully updated.")
-    else:
-        print(f"Failed to update device details. Status code: {response.status_code}")
+    url = "https://timscan.transvolt.in/vehical/post-Device-Details/"
+    headers = {"Content-Type": "application/json"}
+    log_file = "/tmp/cron_job_output.log"
 
-
+    try:
+        response = requests.get(url, headers=headers)
+        with open(log_file, "a") as f:
+            if response.status_code == 200:
+                f.write("Device details successfully updated.\n")
+            else:
+                f.write(f"Failed to update device details. Status code: {response.status_code}\n")
+    except Exception as e:
+        with open(log_file, "a") as f:
+            f.write(f"An error occurred: {str(e)}\n")
