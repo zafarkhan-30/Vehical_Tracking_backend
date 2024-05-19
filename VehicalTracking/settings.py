2e479925e63fa90ff381becbbd7f888634312588
zafarkhan-30 <pubgvila301999@gmail.com>
2024-05-19 15:37:52 +0530

    updated cronjob file

--- VehicalTracking/settings.py
+++ VehicalTracking/settings.py
@@ -163,7 +163,7 @@ AUTH_PASSWORD_VALIDATORS = [
 LANGUAGE_CODE = 'en-us'
 
 USE_TZ = True
-TIME_ZONE = 'Asia/Kolkata'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
--- vehicle/cron_job.py
+++ vehicle/cron_job.py
@@ -1,12 +1,12 @@
 import requests
-
+import certifi
 def run_device_details_view():
     url = "https://timscan.transvolt.in/vehical/post-Device-Details/"
     headers = {"Content-Type": "application/json"}
     log_file = "/tmp/cron_job_output.log"
 
     try:
-        response = requests.get(url, headers=headers)
+        response = requests.get(url, headers=headers , verify=False)
         with open(log_file, "a") as f:
             if response.status_code == 200:
                 f.write("Device details successfully updated.\n")
