1d8378d572e3b4339d36ecf4ab5b19abee767c50
zafarkhan-30 <pubgvila301999@gmail.com>
2024-05-20 13:14:21 +0530

    added Debug true

--- VehicalTracking/settings.py
+++ VehicalTracking/settings.py
@@ -161,12 +161,13 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-
-USE_TZ = True
-TIME_ZONE = 'Asia/kolkata'
+TIME_ZONE =  'Asia/Kolkata'
 
 USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
 
 
 
