1f86dd2ca31afd7792011f71f51ab556f741d18e
zafarkhan-30 <pubgvila301999@gmail.com>
2024-07-03 11:37:46 +0530

    removed Schdlue part

--- VehicalTracking/asgi.py
+++ VehicalTracking/asgi.py
@@ -8,19 +8,22 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 
 import os
+import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from vehicle.routing import websocket_urlpatterns
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'VehicalTracking.settings')
 
-import django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'VehicalTracking.settings')
 django.setup()
 
-asgi_application= get_asgi_application()
+asgi_application = get_asgi_application()
+
 application = ProtocolTypeRouter({
     "http": asgi_application,
     "websocket": AuthMiddlewareStack(
-        URLRouter(websocket_urlpatterns)
+        URLRouter(
+            websocket_urlpatterns
+        )
     ),
 })
